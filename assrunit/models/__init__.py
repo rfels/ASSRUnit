f55b8c101a5dc588837aa6a664df647004aa67da
Robert Fels <fels.robert@gmail.com>
2020-12-18 08:29:55 +0100

    init

--- assrunit/models/__init__.py
+++ assrunit/models/__init__.py
@@ -8,6 +8,10 @@ from assrunit.models.chandelier import (
     ChandelierSimpleModel,
     ChandelierSimpleModelRobust
 )
+from assrunit.models.ac_net import (
+    ACNetModel,
+    ACNetMinimalModel
+)
 from assrunit.models.genesis import (
     GenesisModel,
     BeemanGenesisModel
